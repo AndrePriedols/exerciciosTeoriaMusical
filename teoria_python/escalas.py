2251454815aca91f2f35f69097f0d5f24c8abd1a
AndrePriedols <andrepsouza@gmail.com>
2020-09-30 15:32:07 -0300

    Add files via upload

---
+++ teoria_python/escalas.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os, sys
+
+import random
+
+maior = ['Maior','uníssono','2a Maior','3a Maior','4a Justa','5a Justa','6a Maior','7a Maior']
+menor = ['menor','uníssono','2a Maior','3a menor','4a Justa','5a Justa','6a menor','7a menor']
+menor_harmonica = ['menor harmônica','uníssono','2a Maior','3a menor','4a Justa','5a Justa','6a menor','7a Maior']
+menor_melodica = ['menor melódica','uníssono','2a Maior','3a menor','4a Justa','5a Justa','6a Maior','7a Maior']
+jonio = ['jônio','uníssono','2a Maior','3a Maior','4a Justa','5a Justa','6a Maior','7a Maior']
+dorico = ['dórico','uníssono','2a Maior','3a menor','4a Justa','5a Justa','6a Maior','7a menor']
+frigio = ['frígio','uníssono','2a menor','3a menor','4a Justa','5a Justa','6a menor','7a menor']
+lidio = ['lídio','uníssono','2a Maior','3a Maior','4a Aumentada','5a Justa','6a Maior','7a Maior']
+mixolidio = ['mixolídio','uníssono','2a Maior','3a Maior','4a Justa','5a Justa','6a Maior','7a menor']
+eolio = ['eólio','uníssono','2a Maior','3a menor','4a Justa','5a Justa','6a menor','7a menor']
+locrio = ['lócrio','uníssono','2a menor','3a menor','4a Justa','5a diminuta','6a menor','7a menor']
+
+escalas = [maior, menor, menor_harmonica, menor_melodica, jonio, dorico, frigio, lidio, mixolidio, eolio, locrio]
